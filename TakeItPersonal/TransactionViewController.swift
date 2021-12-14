8ff4fd2b19ccb31b00e22b558b5c45044ebd6bbf
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-13 16:50:48 -0800

    change input method with a bar button alert. enable swipe to delete for transactions.

--- TakeItPersonal/TransactionViewController.swift
+++ TakeItPersonal/TransactionViewController.swift
@@ -52,13 +52,93 @@ class TransactionViewController: UITableViewController {
         return 100
     }
 
+    @IBAction func newLedgerPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add Transactions", message: "", preferredStyle: .alert)
+        var amountTextField = UITextField()
+        var dateTextField = UITextField()
+        var notesTextField = UITextField()
+        
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            
+            let amountString = amountTextField.text
+            
+            if let currentMonth = self.selectedMonth {
+                do{
+                    try self.realm.write{
+                  let newItem = Ledgers()
+                        if let amountDouble = Double(amountString!){
+                            newItem.amount = amountDouble
+                        }else{
+                            newItem.amount = 0.0
+                        }
+                        newItem.notes = notesTextField.text!
+                        newItem.date = dateTextField.text!
+                        currentMonth.ledger.append(newItem)
+                    }
+                }catch{
+                    print("Error creating new Item \(error)")
+                    
+                }
+                self.tableView.reloadData()
+            }
+            
+        }
+        alert.addTextField { (date) in
+            date.placeholder = "12/01"
+            dateTextField = date
+        }
+        alert.addTextField { (amount) in
+            amount.placeholder = "$9.99"
+            amountTextField = amount
+        }
+        
+        alert.addTextField { (notes) in
+            notes.placeholder = "What'd you purchase?"
+            notesTextField = notes
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
     
     func loadItems(){
         transactionsArray = selectedMonth?.ledger.sorted(byKeyPath: "date", ascending: true)
 
         tableView.reloadData()
     }
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let trash = UIContextualAction(style: .destructive,
+                                       title: "Trash") { [weak self] (action, view, completionHandler) in
+            
+            completionHandler(true)
+            let result = self?.delete(at: indexPath) ?? false
+            completionHandler(result)
+        }
+        trash.backgroundColor = .systemRed
+        let configuration = UISwipeActionsConfiguration(actions: [trash])
 
+        return configuration
+    }
+    
+    func delete(at indexPath: IndexPath) -> Bool {
+        // Check if there is a category at provided row
+        guard let categories = transactionsArray?[indexPath.row] else {
+            return false
+        }
+        // Delete data from persistent storage
+        do {
+            // Open transaction
+        try realm.write {
+                // Insert category
+            realm.delete(categories)
+        }
+        } catch {
+            fatalError("Error deleting Category: \(error)")
+        }
+        loadItems()
+           return true
+}
     
     
 }
